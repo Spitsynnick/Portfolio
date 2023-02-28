4b2defbc0671803bac36e5b48274934b0705919a
Spitsynnick <109472664+Spitsynnick@users.noreply.github.com>
2023-02-28 21:38:11 +0300

    Removed extra comments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Добавить класс sticky к header при прокрутке страницы пользователем (header всегда вверху страницы)
+// добавить класс sticky к header при прокрутке страницы пользователем (header всегда вверху страницы)
 const header = document.querySelector(".header");
 
 window.addEventListener("scroll", () => {
@@ -6,7 +6,7 @@ window.addEventListener("scroll", () => {
 })
 
 
-// Реализация выдвижного меню при нажатии на иконку меню
+// реализация выдвижного меню при нажатии на иконку меню
 const menuIcon = document.querySelector(".menu_icon");
 const navigationMenu = document.querySelector(".header_navigation");
 
